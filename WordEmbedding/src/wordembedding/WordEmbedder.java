1d8f98373672547ede72233305803da313284e1f
Mark <math@schantz.com>
2017-01-23 19:29:07 +0100

    Added initial project with maven setup and simple example

---
+++ WordEmbedding/src/wordembedding/WordEmbedder.java
@@ -0,0 +1,52 @@
+package wordembedding;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import org.deeplearning4j.models.word2vec.Word2Vec;
+import org.deeplearning4j.text.sentenceiterator.LineSentenceIterator;
+import org.deeplearning4j.text.sentenceiterator.SentenceIterator;
+import org.deeplearning4j.text.sentenceiterator.SentencePreProcessor;
+import org.deeplearning4j.text.tokenization.tokenizer.preprocessor.CommonPreprocessor;
+import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFactory;
+import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
+
+public class WordEmbedder {
+	public static void main(String[] args) throws IOException {
+	
+        
+        SentenceIterator iter = new LineSentenceIterator(new File("resources/text.txt"));
+        iter.setPreProcessor(new SentencePreProcessor() {
+            @Override
+            public String preProcess(String sentence) {
+                return sentence.toLowerCase();
+            }
+        });
+        
+        TokenizerFactory t = new DefaultTokenizerFactory();
+        t.setTokenPreProcessor(new CommonPreprocessor());
+        
+        Word2Vec vec = new Word2Vec.Builder()
+                .minWordFrequency(5)
+                .iterations(50)
+                .layerSize(100)
+                .seed(42)
+                .windowSize(5)
+                .iterate(iter)
+                .tokenizerFactory(t)
+                .build();
+
+        System.out.println("Fitting Word2Vec model....");
+        
+        vec.fit();
+        
+        Scanner s = new Scanner(System.in);
+        while(true) {
+        	String input = s.nextLine();
+        	System.out.println(Arrays.toString(vec.getWordVector(input)));
+        }
+        
+	}
+}
